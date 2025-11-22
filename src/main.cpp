3fc7b459a7c393a554a38a97be2a512b293bdff9
adrian_mac <adrianfuaad@gmail.com>
2025-11-22 19:19:45 +0800

    Include prerequisite libraries for LCD, WIFI, Arduino, Servo

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,36 @@
+//Arduino Libraries
 #include <Arduino.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// Wifi Libraries + Credentials
+#include <WiFi.h>
+#include <WiFiClientSecure.h>
+#define WIFI_SSID "Daniel@time"
+#define WIFI_PASSWORD "zurina@time"
+
+// Hardware Libraries
+#include <ESP32Servo.h>
+int lcdColumns = 16;
+int lcdRows = 2;
+LiquidCrystal_I2C lcd(0x27, lcdColumns, lcdRows);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Wire.begin();
+  Serial.begin(115200);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+
+  Serial.print("Connecting wifi");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(300);
+  }
+    Serial.println("\nConnected with IP: " + WiFi.localIP().toString());
+
+  // LCD setup
+  lcd.init();
+  lcd.backlight();
+  lcd.print("Speak to give commands!");
 }
 
 void loop() {
